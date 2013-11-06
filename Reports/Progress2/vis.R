68c61352d4c7f54342f868223aceafdf1cbb706e
Matt Delhey <matt.delhey@rice.edu>
2013-11-05 22:25:08 -0600

    begin progress report 2

---
+++ Reports/Progress2/vis.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+err <- read.csv("Submissions/pp1.csv")
+
+library(RColorBrewer)
+pal <- brewer.pal(3, "BuPu")
+
+ggplot(data = err, aes(x = Method), stat = "identity") +
+    geom_bar(aes(y = Leaderboard.Error), fill = pal[3]) +
+    geom_bar(aes(y = Test.Error), fill = pal[2]) +
+    geom_bar(aes(y = Training.Error), fill = pal[1]) +
+    ylab("Error") + xlab("Algorithm") + coord_cartesian(ylim = c(.8, 1))
+
+ggsave("Reports/Progress1/vis.pdf", height = 3, width = 7)
