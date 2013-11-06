06301307691b607c07f57389b998ed1441e95240
Matt Delhey <matt.delhey@rice.edu>
2013-11-06 14:08:43 -0600

    finish progress report 2

--- Reports/Progress2/vis.R
+++ Reports/Progress2/vis.R
@@ -1,13 +1,16 @@
 library(ggplot2)
-err <- read.csv("Submissions/pp1.csv")
+err <- read.csv("Submissions/pp2.csv")
 
 library(RColorBrewer)
 pal <- brewer.pal(3, "BuPu")
+
+err$Method <- reorder(err$Method, -err$Leaderboard.Error)
 
 ggplot(data = err, aes(x = Method), stat = "identity") +
     geom_bar(aes(y = Leaderboard.Error), fill = pal[3]) +
     geom_bar(aes(y = Test.Error), fill = pal[2]) +
     geom_bar(aes(y = Training.Error), fill = pal[1]) +
-    ylab("Error") + xlab("Algorithm") + coord_cartesian(ylim = c(.8, 1))
+    ylab("Error") + xlab("Algorithm") + coord_cartesian(ylim = c(.8, 1)) +
+    theme_bw()
 
-ggsave("Reports/Progress1/vis.pdf", height = 3, width = 7)
+ggsave("Reports/Progress2/vis.pdf", height = 3, width = 7)
