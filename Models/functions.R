bc71108276161aa0569b5cf92b71960b35b80f64
Matt Delhey <matt.delhey@rice.edu>
2013-12-01 17:55:17 -0600

    update functions file

--- Models/functions.R
+++ Models/functions.R
@@ -8,8 +8,19 @@ rmse <- function(y, y_pred, rmsd = FALSE) {
     sqrt(mean((y - y_pred)^2, na.rm = TRUE))
 }
 
+write_predmat <- function(X_out, f_out, write = TRUE, ret = FALSE) {
+    n <- dim(X_out)[1]
+    p <- dim(X_out)[2]
+    bench <- cbind(c(1:(n*p)), c(X_out))
+    colnames(bench) <- c("ID", "Rating")
+    if (write) {
+        write.csv(bench, file = f_out, quote = FALSE, row.names = FALSE)
+    }
+    if (ret) {
+        return(bench)
+    }
+}
+
 cosineDist <- function(x){
   as.dist(1 - x%*%t(x)/(sqrt(rowSums(x^2) %*% t(rowSums(x^2))))) 
 }
-
-
